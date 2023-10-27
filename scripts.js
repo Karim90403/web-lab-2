52d3978abdb099b26c19aff36767e93104b5b728
Карим Музафаров <92757457+Karim90403@users.noreply.github.com>
2023-10-27 11:53:30 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+
+//   $(".navigatorBtn").addEventListener("click", function() {
+//     var browserInfo = "Browser Type: " + navigator.appCodeName + "\n" +
+//                       "Browser Name: " + navigator.appName + "\n" +
+//                       "Browser Version: " + navigator.appVersion;
+//     alert(browserInfo);
+//   });
+
+// });
